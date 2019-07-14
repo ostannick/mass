dcb22ba9df4d20344a71e7448a45e6c8b517246c
ostannick <42483844+ostannick@users.noreply.github.com>
2019-07-14 17:49:17 -0400

    App Layout

--- resources/views/mass/analyze.blade.php
+++ resources/views/mass/analyze.blade.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Lab Tools</title>
-
-        <!--CSS Libraries-->
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.7.6/dist/semantic.min.css">
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-vlOMx0hKjUCl4WzuhIhSNZSm2yQCaf0mOU1hEDK/iztH3gU4v5NMmJln9273A6Jz" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="/css/app.css">
-
-        <!--Javascript Libraries-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.7.6/dist/semantic.min.js"></script>
-        <script src="{{asset('js/init.js')}}"></script>
-
-    </head>
-    <body>
-
-
-
-    <div class="ui container">
+@section('content')
       <h1>{{$protein_name}}</h1>
       <h5>{{\Carbon\Carbon::now()}}</h2>
 
@@ -81,12 +60,6 @@
           <h3>Mass Spectrum</h3>
         </div>
       </div>
-
-
-
     </div>
-
-      </div>
-
-    </body>
-</html>
+  </div>
+@endsection
--- resources/views/mass/mass.blade.php
+++ resources/views/mass/mass.blade.php
@@ -1,114 +1,91 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Lab Tools</title>
+@section('content')
+  <h1>Tryptic Digest Analyzer</h1>
+  <form method="POST" action="/mass">
+    @csrf
+  <div class="ui big form">
+    <div class="field">
+    </div>
 
-        <!--CSS Libraries-->
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.7.6/dist/semantic.min.css">
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-vlOMx0hKjUCl4WzuhIhSNZSm2yQCaf0mOU1hEDK/iztH3gU4v5NMmJln9273A6Jz" crossorigin="anonymous">
-
-        <!--Javascript Libraries-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.7.6/dist/semantic.min.js"></script>
-        <script src="{{asset('js/init.js')}}"></script>
-
-    </head>
-    <body>
-
-      <div class="ui container">
-
-        <h1>Tryptic Digest Analyzer</h1>
-        <form method="POST" action="/mass">
-          @csrf
-        <div class="ui big form">
-          <div class="field">
-          </div>
-
-          <div class="field">
-            <div class="field">
-              <label>Protein Name<i class="fal fa-fw fa-question-circle"></i></label>
-              <input name="protein_name" placeholder="Moraxella bovis n114 LbpB" type="text" value="">
-            </div>
-          </div>
-
-
-            <div class="field">
-              <label>Protein Sequence</label>
-              <textarea class="monospace" name="protein_sequence"></textarea>
-            </div>
-
-          <div class="two fields">
-            <div class="field">
-              <label>Mass Spectrum</label>
-              <textarea class="monospace" name="mass_spectrum"></textarea>
-            </div>
-
-            <div class="field">
-              <label>Peak List</label>
-              <textarea class="monospace" name="peak_list"></textarea>
-            </div>
-          </div>
-
-
-          <div class="three fields">
-            <div class="field">
-              <label>Mass Tolerance (Da)<i class="fal fa-fw fa-question-circle"></i></label>
-              <input name="mass_tolerance" placeholder="Mass Tolerance (Da)" type="text" value="1.5">
-            </div>
-            <div class="field">
-              <label>Peptide Charge State<i class="fal fa-fw fa-question-circle"></i></label>
-              <input name="charge_state" placeholder="Peptide Charge State" type="text" value="1">
-            </div>
-            <div class="field">
-              <label>Intact Mass<i class="fal fa-fw fa-question-circle"></i></label>
-              <input name="intact_mass" placeholder="Intact Mass" type="text" value="">
-            </div>
-          </div>
-
-          <div class="two fields">
-            <div class="field">
-              <label>Lower Mass Cutoff (Da)<i class="fal fa-fw fa-question-circle"></i></label>
-              <input name="cutoff_low" placeholder="500" type="text" value="500">
-            </div>
-            <div class="field">
-              <label>Upper Mass Cutoff (Da)<i class="fal fa-fw fa-question-circle"></i></label>
-              <input name="cutoff_high" placeholder="4000" type="text" value="4000">
-            </div>
-          </div>
-
-          <div class="two fields">
-            <div class="field">
-              <label>Matrix</label>
-                <div class="ui selection dropdown">
-                    <input type="hidden" name="matrix">
-                    <i class="dropdown icon"></i>
-                    <div class="default text">Matrix</div>
-                    <div class="menu">
-                        <div class="item" data-value="1" selected>a-4-cyano-hydroxycinaminnic acid (HCCA)</div>
-                        <div class="item" data-value="0">Sinnapinic Acid (SA)</div>
-                        <div class="item" data-value="0">Dihydroxybenzoic Acid (DHB)</div>
-                    </div>
-                </div>
-            </div>
-          </div>
-
-          <div class="ui message">
-            <div class="header">Notes:</div>
-            <ul class="list">
-              <li>Proteins assumed to be treated with iodoacetamide</li>
-              <li>Proteins assumed to be treated with trypsin</li>
-            </ul>
-          </div>
-
-          <button type="submit" class="ui submit button">Submit</button>
-
-        </div>
-      </form>
-
-
+    <div class="field">
+      <div class="field">
+        <label>Protein Name<i class="fal fa-fw fa-question-circle"></i></label>
+        <input name="protein_name" placeholder="Moraxella bovis n114 LbpB" type="text" value="">
       </div>
-    </body>
-</html>
+    </div>
+
+
+      <div class="field">
+        <label>Protein Sequence</label>
+        <textarea class="monospace" name="protein_sequence"></textarea>
+      </div>
+
+    <div class="two fields">
+      <div class="field">
+        <label>Mass Spectrum</label>
+        <textarea class="monospace" name="mass_spectrum"></textarea>
+      </div>
+
+      <div class="field">
+        <label>Peak List</label>
+        <textarea class="monospace" name="peak_list"></textarea>
+      </div>
+    </div>
+
+
+    <div class="three fields">
+      <div class="field">
+        <label>Mass Tolerance (Da)<i class="fal fa-fw fa-question-circle"></i></label>
+        <input name="mass_tolerance" placeholder="Mass Tolerance (Da)" type="text" value="1.5">
+      </div>
+      <div class="field">
+        <label>Peptide Charge State<i class="fal fa-fw fa-question-circle"></i></label>
+        <input name="charge_state" placeholder="Peptide Charge State" type="text" value="1">
+      </div>
+      <div class="field">
+        <label>Intact Mass<i class="fal fa-fw fa-question-circle"></i></label>
+        <input name="intact_mass" placeholder="Intact Mass" type="text" value="">
+      </div>
+    </div>
+
+    <div class="two fields">
+      <div class="field">
+        <label>Lower Mass Cutoff (Da)<i class="fal fa-fw fa-question-circle"></i></label>
+        <input name="cutoff_low" placeholder="500" type="text" value="500">
+      </div>
+      <div class="field">
+        <label>Upper Mass Cutoff (Da)<i class="fal fa-fw fa-question-circle"></i></label>
+        <input name="cutoff_high" placeholder="4000" type="text" value="4000">
+      </div>
+    </div>
+
+    <div class="two fields">
+      <div class="field">
+        <label>Matrix</label>
+          <div class="ui selection dropdown">
+              <input type="hidden" name="matrix">
+              <i class="dropdown icon"></i>
+              <div class="default text">Matrix</div>
+              <div class="menu">
+                  <div class="item" data-value="HCCA" selected>a-4-cyano-hydroxycinaminnic acid (HCCA)</div>
+                  <div class="item" data-value="SA">Sinnapinic Acid (SA)</div>
+                  <div class="item" data-value="DHB">Dihydroxybenzoic Acid (DHB)</div>
+              </div>
+          </div>
+      </div>
+    </div>
+
+    <div class="ui message">
+      <div class="header">Notes:</div>
+      <ul class="list">
+        <li>Proteins assumed to be treated with iodoacetamide</li>
+        <li>Proteins assumed to be treated with trypsin</li>
+      </ul>
+    </div>
+
+    <button type="submit" class="ui submit button">Submit</button>
+
+  </div>
+</form>
+@endsection
